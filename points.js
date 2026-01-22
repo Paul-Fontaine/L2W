679cf8036476dbcf3b7002aee0f9157ac5c5aed2
Paul Fontaine <paul.fontaine@isen-ouest.yncrea.fr>
2026-01-22 17:01:56 +0100

    add points to user

--- points.js
+++ points.js
@@ -1,12 +1,36 @@
+const dbName = "users";
+
+function saveData(data) {
+    localStorage.setItem(dbName, JSON.stringify(data));
+}
+
+function getData() {
+    const data = localStorage.getItem(dbName);
+    if (data) {
+        return JSON.parse(data);
+    } else {
+        console.log("no users")
+        return []
+    }
+}
+
+function addPoints(email, points = 1) {
+    const users = getData(); 
+
+    const user = users.find(u => u.email === email);
+    
+    if (user) {
+        user.points += points;
+        saveData(users);
+    } else {
+        console.warn(`User with email ${email} not found. can't add points`);
+    }
+}
 
 raw_user = localStorage.getItem("current_user");
-
 if (raw_user) {
     current_user = JSON.parse(raw_user);
-}
-function addPoints(userKey, points) {
-
-    current_user.points += points;
+    addPoints(current_user.email, points=100);
 }
 
 function getBadges(points) {
